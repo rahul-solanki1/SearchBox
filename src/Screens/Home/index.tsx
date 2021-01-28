3a6fe3599c524b38e7d0ca5e7ec464cab4003e02
rahulsolanki <rsolanki@deqode.com>
2021-01-28 19:37:13 +0530

    Updated home screen with autocomplete features
    - Added onFocus & on focus change on text input
    - Implemented get suggestions mock api result
    - Added empty check

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,8 +1,69 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Keyboard, StyleSheet, View} from 'react-native';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
+import {AutocompleteInput} from '../../Components';
+import {MockAPI} from '../../Services/API';
+import {helper} from '../../Util';
 
 const Home: React.FC<{}> = () => {
-  return <View style={styles.container} />;
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  useEffect(() => {
+    const word = helper.getLastWord(value);
+
+    if (word.length === 0) {
+      setSuggestions([]);
+      return;
+    }
+
+    getSuggestions(word);
+  }, [value]);
+
+  const getSuggestions = (word: string) => {
+    MockAPI.getSuggestions(word)
+      .then((values) => {
+        let suggestionList = values as string[];
+        console.log(suggestionList);
+        setSuggestions(suggestionList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const onSuggestionSelected = (item: string) => {
+    setSuggestions([]);
+
+    setValue(
+      value.substr(0, value.lastIndexOf(helper.getLastWord(value))) + item,
+    );
+  };
+
+  const onFocusOut = () => {
+    Keyboard.dismiss();
+    setSuggestions([]);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback
+        containerStyle={styles.container}
+        accessible={false}
+        onPress={() => onFocusOut()}>
+        <AutocompleteInput
+          value={value}
+          highlightWord={helper.getLastWord(value)}
+          suggestion={suggestions}
+          onValueChange={setValue}
+          onFocus={() =>
+            value.length && getSuggestions(helper.getLastWord(value))
+          }
+          onSuggestionSelected={onSuggestionSelected}
+        />
+      </TouchableWithoutFeedback>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
